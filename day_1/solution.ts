fbffb83349a3e64dcdcb4cbd5dfec397b5b486d9
Tatiana <hello@tatianacodes.com>
2021-12-01 21:28:25 -0800

    Add day 1 solution

---
+++ day_1/solution.ts
@@ -0,0 +1,33 @@
+const fs = require('fs')
+const path = require('path');
+const filePath = path.join(__dirname, 'input.txt');
+
+let inputData: number[];
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  inputData = data.split('\n').map(n => parseInt(n));
+  console.log(processData(inputData))
+})
+
+function processData(input: number[]) {
+  return input.reduce((prev, curr, index, arr) => {
+    if (index === 0 || arr[index] === arr[index - 1]) {
+      return prev
+    } else if (arr[index] > arr[index - 1]) {
+      return ({
+        "increase": prev["increase"] += 1,
+        "decrease": prev["decrease"]
+      })
+    } else {
+      return ({
+        "increase": prev["increase"],
+        "decrease": prev["decrease"] += 1
+      })
+    }
+  }, { increase: 0, decrease: 0 })
+}
+
