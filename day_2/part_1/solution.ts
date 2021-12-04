610bfcd32fe3862aa71d0af1c6477e01f7c637f8
Tatiana <hello@tatianacodes.com>
2021-12-04 01:58:10 -0800

    Add part 1 day 2 solution:

--- day_2/part_1/solution.ts
+++ day_2/part_1/solution.ts
@@ -1,3 +1,5 @@
+import { access } from "fs";
+
 const fs = require('fs')
 const path = require('path');
 const filePath = path.resolve('../day_2', '../input.txt');
@@ -10,11 +12,25 @@ fs.readFile(filePath, 'utf8', (err: Error, data: string) => {
     return
   }
   inputData = data.split('\n')
-  console.log(filePath)
-  console.log(inputData)
-  // console.log(processData(inputData))
+  console.log(processData(inputData))
 })
 
-const processData = (data) => {
+interface accumulator {
+  [index: string]: number
+}
 
+const processData = (data: string[]) => {
+  const result = data.reduce((acc: accumulator, curr: string) => {
+    const direction = curr.split(" ");
+    return {
+      ...acc,
+      [direction[0]]: acc[direction[0]] + parseInt(direction[1])
+    }
+  }, {
+    forward: 0,
+    down: 0,
+    up: 0,
+  })
+
+  return (result.down - result.up) * result.forward
 }
